02695bb912049e4d996dc317031ba8c0f78af97d
CarlosMiraGarcia <carlosgarciamira@gmail.com>
2020-12-30 13:40:44 +0000

    Add project files.

---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    public class Node
+    {
+        public int Data { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+        public Node(int data)
+        {
+            this.Data = data;
+        }
+        public void Insert(int value)
+        {
+            if (value <= Data)
+            {
+                if (Left == null)
+                {
+                    Left = new Node(value);
+                }
+
+                else
+                {
+                    Left.Insert(value);
+                }
+            }
+
+            else
+            {
+                if (Right == null)
+                {
+                    Right = new Node(value);
+                }
+
+                else
+                {
+                    Right.Insert(value);
+                }
+            }
+        }
+        public bool Contains(int value)
+        {
+            if (value == Data)
+            {
+                return true;
+            }
+
+            else if (value < Data)
+            {
+                if (Left == null)
+                { 
+                    return false;
+                }
+
+                else
+                {
+                    return Left.Contains(value);
+                }
+            }
+
+            else
+            {
+                if (Right == null)
+                {
+                    return false;
+                }
+
+                else
+                {
+                    return Right.Contains(value);
+                }
+            }
+        } 
+        public void PrintInOrder()
+        {
+            if (Left != null)
+            {
+                Left.PrintInOrder();
+            }
+
+            Console.WriteLine(Data);
+
+            if (Right != null)
+            {
+                Right.PrintInOrder();
+            }
+        }
+    }
+}
---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node root = new Node(50);
+            Random rdn = new Random();
+
+            for (int i = 0; i < 20; i++)
+            {
+                root.Insert(rdn.Next(0, 100));
+            }
+
+            root.PrintInOrder();
+            Console.Write("Does the tree contains 5000? ");
+            Console.WriteLine(root.Contains(5000));
+            Console.Write("Does the tree contains 2101? ");
+            Console.WriteLine(root.Contains(2101));
+            Console.ReadLine();
+
+            //Checking if it is a binary tree
+            Console.WriteLine("Is a proper Binary Tree? " + CheckBST(root));
+            //Adding a bigger number than root on the left
+            Node left = new Node(5001);
+            root.Left = left;
+            Console.WriteLine("Is a proper Binary Tree? " + CheckBST(root));
+            
+        }
+
+        // CheckBST will basically check if the tree is a valid binary tree
+        // That means that the nodes on the left of the root as smaller than the root
+        // and the nodes on the right side of the root are bigger than the root
+        static bool CheckBST(Node root, int min, int max)
+        {
+            if (root == null)
+            {
+                return true;
+            }
+
+            if (root.Data < min || root.Data > max)
+            {
+                return false;
+            }
+
+            return CheckBST(root.Left, min, root.Data - 1) && CheckBST(root.Right, root.Data + 1, max);
+        }
+        static bool CheckBST(Node root)
+        {
+            return CheckBST(root, int.MinValue, int.MaxValue);
+        }
+    }
+
+    public class BST
+    { }
+
+}
